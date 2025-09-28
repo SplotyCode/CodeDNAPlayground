1affc697ac9ecc073bc0e672c0491edd86cfbde5
David Scandurra <david.scandurra@check24.de>
2025-09-28 03:39:42 +0200

    Build fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,9 +20,21 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(23)
 }
+
 application {
     mainClass.set("de.scandurra.codedna.AppKt")
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = "de.scandurra.codedna.AppKt"
+    val dependencies = configurations
+        .runtimeClasspath
+        .get()
+        .map(::zipTree)
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
